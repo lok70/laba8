f6241b078b3c9e144568cc8753b685dc8fd4ec74
Павлов <lokwer321@gmail.com>
2022-10-06 12:56:45 +0300

    показывает високосность

---
+++ god.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var n := readinteger('введите номер года');
+  assert(n>=0);
+  if (n mod 4 = 0) and (n mod 100 <> 0) then
+    print('високосный')
+  else
+    print('Невисокосный');
+end.
